cc68faf455f5f352b7c2afa4092ef293a60315cc
Dean Camenzuli <dcamenzuli@gmail.com>
2018-07-30 10:36:39 +1000

    Add files via upload

---
+++ DynatraceRUMDetails.ps1
@@ -0,0 +1,96 @@
+# Author: Dean Camenzuli
+# Revision: v1.0
+# Date: 30/03/2018
+#
+# Requirements: Access Dynatrace User Session Query endpoint.
+# Convert incoming JSON to parsable flat CSV file.
+# Endgoal: Be able to capture user session data and detailed timings (per browser) for each user action
+# Script has been tested running on PowerShell v3
+#
+
+# ChangeLog
+# 1.0 | 30/03/2018 | Dean Camenzuli | Initial creation
+#
+
+# Query used in API explorer
+# SELECT userAction.Name, browserType, browserFamily, browserMajorVersion, AVG(userAction.networkTime), AVG(userAction.serverTime), AVG(userAction.frontendTime), AVG(userAction.visuallyCompleteTime), COUNT(*) FROM usersession GROUP BY userAction.Name, browserType, browserFamily, browserMajorVersion
+# 
+
+# helper function to turn PSCustomObject into a list of key/value pairs
+function Get-ObjectMembers {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [AllowEmptyString()]
+        [PSCustomObject]$obj
+    )
+    $obj | Get-Member -MemberType NoteProperty | ForEach-Object {
+        $key = $_.Name
+        [PSCustomObject]@{Key = $key; Value = $obj."$key"
+        }
+    }
+}
+
+$fileOut = "API Export.csv"
+$fileOutput = @()
+
+# Grab the jsonRequestURL (generated through API explorer) and convert it into a PSCustomObject
+$web_client = new-object System.Net.WebClient
+$jsonRequestURL = "[API REQUEST URL]"
+$jsonContents = $web_client.DownloadString($jsonRequestURL) | ConvertFrom-Json
+
+"API Call Completed and loaded into PSCustomObject"
+
+# Convert the first part of the PSCustomObject into Key/Value Pairs
+$jsonEntities = $jsonContents.values | Get-ObjectMembers
+
+# Begin looping through all the items in the custom object to the correct variable names to print the rows later
+foreach($i in $jsonEntities)
+{
+    $userActionName = $i.key
+    $city = ""
+    $userCount = ""
+
+    $jsonInner01 = $i.value | Get-ObjectMembers
+
+    foreach($a in $jsonInner01)
+    {
+        $browserType = $a.key
+
+        $jsonInner02 = $a.value | Get-ObjectMembers
+
+        foreach($b in $jsonInner02)
+        {
+            $browserFamily = $b.key
+
+            $jsonInner03 = $b.value | Get-ObjectMembers
+
+            foreach($c in $jsonInner03)
+            {
+                $browserMajorVersion = $c.key
+
+                $networkTime = $c.value[0]
+                $serverTime = $c.value[1]
+                $frontendTime = $c.value[2]
+                $visuallyCompleteTime = $c.value[3]
+                $sessionCount = $c.value[4]
+
+                # Assign the collected values to a row for export to excel
+                $row = New-Object PSObject
+                $row | Add-Member -MemberType NoteProperty -Name 'User Action Name' -Value $userActionName
+                $row | Add-Member -MemberType NoteProperty -Name 'Browser Type' -Value $browserType
+                $row | Add-Member -MemberType NoteProperty -Name 'Browser Family' -Value $browserFamily
+                $row | Add-Member -MemberType NoteProperty -Name 'Browser Version' -Value $browserMajorVersion
+                $row | Add-Member -MemberType NoteProperty -Name 'Network Time' -Value $networkTime
+                $row | Add-Member -MemberType NoteProperty -Name 'Server Time' -Value $serverTime
+                $row | Add-Member -MemberType NoteProperty -Name 'Frontend Time' -Value $frontendTime
+                $row | Add-Member -MemberType NoteProperty -Name 'Visually Complete Time' -Value $visuallyCompleteTime
+                $row | Add-Member -MemberType NoteProperty -Name 'User Session Count' -Value $sessionCount
+                $fileOutput += $row
+            }
+        }
+    }
+}
+
+# Export to csv
+$fileOutput | Export-CSV $fileOut -NoTypeInformation
